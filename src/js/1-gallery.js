f9979fad1b3b1bdf245978af8f54d99cbf1dbd5c
Lakanna <annalakus@ukr.net>
2024-07-26 00:19:41 +0100

    2601

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,7 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+
 const images = [
     {
       preview:
@@ -66,4 +70,30 @@ const images = [
   
   const galleryList = document.querySelector(".gallery");
 
+  console.log(galleryList);
+
+  function createMarkup(arr) {
+    return arr
+      .map(
+        ({ preview, original, description }) => `
+       <li class="gallery-item">
+	     <a class="gallery-link" href=${original}>
+	  	   <img 
+	  	   	  class="gallery-image" 
+	  	   	  src=${preview} 
+	  	   	  alt=${description} 
+	  	   	/>
+	     </a>
+       </li>`
+      )
+      .join("");
+  }
   
+  galleryList.insertAdjacentHTML("afterbegin", createMarkup(images));
+
+//   console.log(simpleLightbox);
+
+  const galleryByLitebox = new SimpleLightbox('.gallery a', { captionsData: "alt" , captionDelay: 250}); 
+
+  galleryByLitebox.show()
+
